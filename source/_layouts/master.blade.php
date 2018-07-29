08dbbcc1209c8381593a50eccada159e2830c97e
Kristoffer Högberg <krihog@gmail.com>
2018-07-29 14:39:19 +0200

    Add title to html header, including page title if available

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -5,10 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
+
+    <title>{{ $page->title ? $page->title . ' | hmazter.com' : 'hmazter.com' }}</title>
 </head>
 <body>
 <a href="/"><h1 class="site-header">Hmazter.com</h1></a>
 
 @yield('body')
+
 </body>
 </html>
