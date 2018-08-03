b7d817a5f8be67de33755615812576bc0a8a92f9
Kristoffer Högberg <krihog@gmail.com>
2018-08-03 22:39:11 +0200

    Paginate through posts on index page

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,16 +1,44 @@
+---
+pagination:
+    collection: posts
+    perPage: 3
+---
 @extends('_layouts.master')
 
 @section('body')
     <section class="section">
-        <h2>Posts</h2>
-        <ul>
-            @foreach ($posts as $post)
+        @foreach ($pagination->items as $post)
+            <div class="content post">
+                <h1>{{ $post->title }}</h1>
+
+                <div>{!! $post->getContent() !!}</div>
+
+                <p class="author">This article was posted by {{ $post->author }} at {{ $post->date }}</p>
+            </div>
+            <hr>
+        @endforeach
+    </section>
+
+    <nav class="pagination is-centered" role="navigation" aria-label="pagination">
+        @if ($previous = $pagination->previous)
+            <a href="{{ $page->baseUrl }}{{ $previous }}" class="pagination-previous">Previous</a>
+        @else
+            <a class="pagination-previous" disabled>previous</a>
+        @endif
+
+        @if ($next = $pagination->next)
+            <a href="{{ $page->baseUrl }}{{ $next }}" class="pagination-next">Next</a>
+        @endif
+
+        <ul class="pagination-list">
+            @foreach ($pagination->pages as $pageNumber => $path)
                 <li>
-                    <a href="{{ $post->getUrl() }}">
-                        {{ $post->title }}
+                    <a href="{{ $page->baseUrl }}{{ $path }}"
+                       class="pagination-link  {{ $pagination->currentPage == $pageNumber ? 'is-current' : '' }}">
+                        {{ $pageNumber }}
                     </a>
                 </li>
             @endforeach
         </ul>
-    </section>
+    </nav>
 @endsection
