e16982466a88f4958a2976df93dbdca1956c31cd
Kristoffer Högberg <krihog@gmail.com>
2018-08-11 11:38:11 +0200

    Add google-site-verification meta tag

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="google-site-verification" content="FG8byi9oZ_6yNU_9PSFgl-mW21rF5Bx27mDIUDQRmiA" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css"
           integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
