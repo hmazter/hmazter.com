b60d5700794112a7dad6a444fa7091b26177c471
Kristoffer Högberg <krihog@gmail.com>
2018-08-25 11:53:33 +0200

    Add favicons and listener to copy to root on build (#15)

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -16,6 +16,14 @@
     <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
 
     <link rel="alternate" type="application/rss+xml" title="hmazter.com Feed" href="{{ $page->baseUrl }}/feed.xml" />
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
 </head>
 <body>
 
